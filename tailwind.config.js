7c22ea5017593e700eb875a9a9a8b4aeda0db0a3
Kevin Krummnacker <kevin.krummnacker@gmail.com>
2021-10-30 03:57:36 +0200

    feat: basic vue3 boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: { 
+    content: ['./public/**/*.html', './src/**/*.vue'] 
+  },
+  mode: 'jit',
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
+const progressBarPlugin = require('progress-bar-webpack-plugin')
+const bundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const chalk = require('chalk')
+
+const progressBarFormat = `${chalk.green.bold('Building')} [:bar] ${chalk.yellow.bold(':percent')} (:elapsed seconds)`
+const publicPath = process.env.BASE_URL || '/';
+const development = process.env.NODE_ENV !== "production";
+
+const configureWebpack = config => {
+  if (!development && process.env.npm_lifecycle_event === 'analyze') {
+    config.plugins.push(new bundleAnalyzerPlugin({
+      isModernBuild: process.env.VUE_CLI_MODERN_BUILD === "true"
+    }))
+  }
+  config.plugins.push(new progressBarPlugin({ 
+    format: progressBarFormat,
+    clear: false
+  }))
+}
+
 module.exports = {
-  transpileDependencies: true
+  publicPath,
+
+  lintOnSave: false,
+  transpileDependencies: true,
+
+  configureWebpack
 }
